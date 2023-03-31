1479c01e1e55908849d0d352cfda613c198319ca
Ravi Github <Ravishankaratavalgi.1999@gmail.com>
2023-03-31 16:38:42 +0530

    code commit

--- src/aws/com/aws_commit/HelloWorld.java
+++ src/aws/com/aws_commit/HelloWorld.java
@@ -3,6 +3,6 @@ package aws.com.aws_commit;
 public class HelloWorld {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!!")
+        System.out.println("Hello World!!");
     }
 }
